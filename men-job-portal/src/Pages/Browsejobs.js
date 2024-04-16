cbc6708898836691bf0bb72aff1e71c1f175a161
MHaris03 <xhkharis2@gmail.com>
2024-04-16 15:48:07 +0500

    Add browsejob page

--- men-job-portal/src/Pages/Browsejobs.js
+++ men-job-portal/src/Pages/Browsejobs.js
@@ -1,11 +1,190 @@
-import React from 'react'
+import React from 'react';
 
-const Browsejobs = () => {
+const BrowseJobs = () => {
   return (
-    <div>
-      <h2 className='mb-20 xl:px-34 px-40 md:py-20 py-50 text-2xl container text-bold text-sky-500 font-sans text-center'>Browse Jobs Coming Soon!</h2>
-    </div>
-  )
-}
+    <>
+      <div className='flex justify-center text-center'>
+        <h2 className='mb-10 text-2xl container text-bold text-sky-500 font-sans text-center'>
+          Browse Jobs
+        </h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 items-center justify-center mb-10 px-10">
+        {/* First div */}
+        <div className=" bg-gray-100 p-4 rounded-lg">
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg'>
+            <h3 className="text-sm font-bold ">
+              Hospitality Jobs
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              3
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Engineering Jobs
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>6</p>
+          </div> <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Oil & Gas
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>1</p>
+          </div> <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Administration
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>3</p>
+          </div> <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              IT & Software
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>3</p>
+          </div> <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Technology
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>3</p>
+          </div> <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Government
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>3</p>
+          </div> <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Accounting / Finance
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>3</p>
+          </div>
+        </div>
+        {/* Second div */}
+        <div className=" bg-gray-100 p-4 rounded-lg">
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg'>
+            <h3 className="text-sm font-bold ">
+              Construction / Facilities
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              13
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Telecom
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              10
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Design & Multimedia
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              10
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Human Resource
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              25
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Consumer
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              20
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Manufacturing
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              12
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Retail
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              3
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Distribution/Logistics
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              3
+            </p>
+          </div>
+        </div>
+        {/* Third div */}
+        <div className=" bg-gray-100 p-4 rounded-lg">
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg'>
+            <h3 className="text-sm font-bold ">
+              Supply Chain Operations
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              13
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Healthcare & Medical
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              10
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Procurement / Sourcing
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              10
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Information Technology (IT)
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              25
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Sales/Business Development
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              20
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Legal & Professional Services
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              12
+            </p>
+          </div>
+          <div className='flex justify-between  bg-white py-3 px-3 rounded-lg mt-3'>
+            <h3 className="text-sm font-bold ">
+              Life Sciences & Healthcare
+            </h3>
+            <p className='bg-[#e7f8fd] text-[#98d8f4] px-2'>
+              3
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Browsejobs
+export default BrowseJobs;
